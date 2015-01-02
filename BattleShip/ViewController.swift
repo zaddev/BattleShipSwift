13384bd0c5c9cf6a3535fc96445be1f9c0ab73f2
Ruben van den Engel <rubenxvde@gmail.com>
2015-01-02 18:03:57 +0100

    1st
    Daar zijn we dan. Nog een lange weg te gaan maar we krijgen voor het
    eerst antwoord van de node js server

--- BattleShip/ViewController.swift
+++ BattleShip/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let host = "http://localhost:3700/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        connectToHost()
+        
+    }
+
+    private func connectToHost() {
+        SIOSocket.socketWithHost(host, reconnectAutomatically: true, attemptLimit: 0, withDelay: 1, maximumDelay: 5, timeout: 20, response: {socket in
+            socket.onConnect = {
+                println("Connected to \(self.host)")
+                //socket.emit("add user", args: [self.username])
+            }
+            socket.on("getSchepen", callback: {(AnyObject data) -> Void in
+                let json = JSON(data)
+                
+                println(json)
+                println(json[0][1]["width"])
+                
+                //socket.emit("new message", args: [self.message])
+            })
+            socket.onDisconnect = {
+                println("Disconnected from \(self.host)")
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,4 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+
+pod 'SIOSocket', '~> 0.2.0'
